1880764c2a3b4c8d239c418ee8cff674db80b3ab
Sholycul <michaeloluwasholaiji2020@gmail.com>
2024-09-10 05:09:14 -0700

    Flask homepage

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""Module for running forst instance of our app"""
+
+from flask import Flask, render_template
+
+app = Flask(_name_)
+
+
+@app.route("/")
+def index():
+    """Home page"""
+    return render_templat('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
